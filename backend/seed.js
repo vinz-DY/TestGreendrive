41545c70d4396800926b2bedc23eea9ae1557fc2
Julien RICHARD <julien.y.richard@gmail.com>
2023-12-19 09:27:45 +0100

    Dispatch des datas dans des fichiers séparés et SOLID du seed, nettoyage de commentaire inutile

--- backend/seed.js
+++ backend/seed.js
@@ -6,6 +6,10 @@ require("dotenv").config();
 // Import Faker library for generating fake data
 const { faker } = require("@faker-js/faker");
 
+const terminal = require("./database/terminal");
+const connectic = require("./database/connectic");
+const state = require("./database/state");
+
 // Import database client
 const database = require("./database/client");
 
@@ -15,31 +19,9 @@ const seed = async () => {
     // See why here: https://eslint.org/docs/latest/rules/no-await-in-loop
 
     /* ************************************************************************* */
-
     // Generating Seed Data
 
-    // Optional: Truncate tables (remove existing data)
-    // await database.query("truncate item");
-
-    // Insert fake data into the 'item' table
-    // for (let i = 0; i < 5; i += 1) {
-    //   queries.push(
-    //     database.query(
-    //       "insert into player(role_id,username, email, password) values (?,?,?,?)",
-    //       [
-    //         1,
-    //         faker.internet.userName(),
-    //         faker.internet.email(),
-    //         faker.internet.password(),
-    //       ]
-    //     )
-    //   );
-    // }
     const queriesState = [];
-    const state = [
-      "insert into state(label) VALUES ('non active')",
-      "insert into state(label) VALUES ('active')",
-    ];
     for (let i = 0; i < state.length; i += 1) {
       queriesState.push(database.query(state[i]));
     }
@@ -47,10 +29,6 @@ const seed = async () => {
     await Promise.all(queriesState);
 
     const queriesConnectic = [];
-    const connectic = [
-      "insert into connectic(plugType) VALUES ('T2')",
-      "insert into connectic(plugType) VALUES ('EF')",
-    ];
     for (let i = 0; i < connectic.length; i += 1) {
       queriesConnectic.push(database.query(connectic[i]));
     }
@@ -72,18 +50,9 @@ const seed = async () => {
     }
     await Promise.all(queriesUser);
 
+    // .........INSERT TABLE Terminal ............................................
     const queriesTerminal = [];
-    const terminal = [
-      "insert into terminal(city,adresseStation,xlongitude,ylatitude,power,acces_recharge,access,localisation,region, connectic_id, state_id) VALUES ('MARSEILLE-8E - Place Théo Lombard','Place Théo Lombard 13008 MARSEILLE-8E',5.383026,43.27085,'22','payant','24h/24 7j/7','43.27085,5.383026',NULL,1,2)",
-      "insert into terminal(city,adresseStation,xlongitude,ylatitude,power,acces_recharge,access,localisation,region, connectic_id, state_id) VALUES ('MARSEILLE-3E - 28 Rue Jobin','28 Rue Jobin 0 MARSEILLE-3E',5.389845,43.309831,'22','payant','24h/24 7j/7','43.309831,5.389845',NULL,1,2)",
-      "insert into terminal(city,adresseStation,xlongitude,ylatitude,power,acces_recharge,access,localisation,region, connectic_id, state_id) VALUES ('MARSEILLE-16E - Boulevard D''Annam','Boulevard D''Annam 0 MARSEILLE-16E',5.329846,43.361274,'22','payant','24h/24 7j/7','43.361274,5.329846',NULL,1,2)",
-      "insert into terminal(city,adresseStation,xlongitude,ylatitude,power,acces_recharge,access,localisation,region, connectic_id, state_id) VALUES ('MARSEILLE-2E - 29 Boulevard De Dunkerque','29 Boulevard De Dunkerque 0 MARSEILLE-2E',5.367851,43.308184,'22','payant','24h/24 7j/7','43.308184,5.367851',NULL,1,2)",
-      "insert into terminal(city,adresseStation,xlongitude,ylatitude,power,acces_recharge,access,localisation,region, connectic_id, state_id) VALUES ('MARSEILLE-9E - 83-81 Boulevard Du Redon','83-81 Boulevard Du Redon 0 MARSEILLE-9E',5.421871,43.253283,'22','payant','24h/24 7j/7','43.253283,5.421871',NULL,1,2)",
-      "insert into terminal(city,adresseStation,xlongitude,ylatitude,power,acces_recharge,access,localisation,region, connectic_id, state_id) VALUES ('MARSEILLE - Parking De La Mairie, Bd Bouyala D''Arnaud','Parking De La Mairie, Bd Bouyala D''Arnaud 13000 MARSEILLE',5.443135,43.297307,'4','payant','24h/24 7j/7','43.297307,5.443135',NULL,1,2)",
-      "insert into terminal(city,adresseStation,xlongitude,ylatitude,power,acces_recharge,access,localisation,region, connectic_id, state_id) VALUES ('AIX-EN-PROVENCE - Avenue Jean Orsini - Parking De La Salle Des Fêtes De Puyricard','Avenue Jean Orsini - Parking De La Salle Des Fêtes De Puyricard 13540 AIX-EN-PROVENCE',5.422441,43.580561,'22','payant','24h/24 7j/7','43.580561,5.422441',NULL,1,2)",
-      "insert into terminal(city,adresseStation,xlongitude,ylatitude,power,acces_recharge,access,localisation,region, connectic_id, state_id) VALUES ('AIX-EN-PROVENCE - Cours Saint-Louis','Cours Saint-Louis 13090 AIX-EN-PROVENCE',5.453065,43.529819,'22','payant','24h/24 7j/7','43.529819,5.453065',NULL,1,2)",
-      "insert into terminal(city,adresseStation,xlongitude,ylatitude,power,acces_recharge,access,localisation,region, connectic_id, state_id) VALUES ('ISTRES - Gymnase Donadieu','Gymnase Donadieu 13800 ISTRES',4.988791,43.507643,'18','payant','24h/24 7j/7','43.507643,4.988791',NULL,1,2)",
-    ];
+
     for (let i = 0; i < terminal.length; i += 1) {
       queriesTerminal.push(database.query(terminal[i]));
     }
