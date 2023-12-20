c7893cf227a40abe36870257a0d0b0c3f79b9435
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-12-20 11:56:51 +0100

    ajout des informations sur la borne et modification du nom de ma const

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -6,7 +6,7 @@ import "./Map.css";
 import { useLoaderData } from "react-router-dom";
 
 function Map() {
-  const data = useLoaderData();
+  const terminalsData = useLoaderData();
   return (
     <div>
       <h1>Test</h1>
@@ -16,15 +16,19 @@ function Map() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <MarkerClusterGroup chunkedLoading>
-          {data.map((marker) => (
+          {terminalsData.map((marker) => (
             <Marker
               position={[marker.ylatitude, marker.xlongitude]}
               key={marker.id}
             >
               <Popup>
+                <h2>Informations sur la borne</h2>
                 <ul>
                   <li>Adresse: {marker.city}</li>
                   <li>Acessibilité: {marker.access}</li>
+                  <li>Type de prise: {marker.connectic_id}</li>
+                  <li>Puissance maximale: {marker.power}</li>
+                  <li>Accès à la borne: {marker.acces_recharge}</li>
                 </ul>
               </Popup>
             </Marker>
