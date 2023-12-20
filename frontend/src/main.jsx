90a0878c05ace1c798dcac0a96b61dec297841d7
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-12-20 11:42:17 +0100

    création du path map

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Map from "./components/Map/Map";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/map",
+    element: <Map />,
+    loader: () => {
+      return axios
+        .get(`${import.meta.env.VITE_BACKEND_URL}/api/terminals`)
+        .then((response) => response.data)
+        .catch((err) => console.error(err));
+    },
   },
 ]);
 
