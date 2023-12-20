0e378e4bf2da70562c47906a287d4dfe6df83122
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-12-20 11:40:46 +0100

    ajout du TerminalManager

--- backend/src/tables.js
+++ backend/src/tables.js
@@ -4,9 +4,11 @@
 
 // Import the manager modules responsible for handling data operations on the tables
 const ItemManager = require("./models/ItemManager");
+const TerminalManager = require("./models/TerminalManager");
 
 const managers = [
   ItemManager,
+  TerminalManager,
   // Add other managers here
 ];
 
