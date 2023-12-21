dce2526b946eb55cb25430fea374a3641fda6434
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-12-21 14:51:36 +0100

    suppression de l'appel du composant Map car on ne l'affiche que sur la route /map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import Map from "./components/Map/Map";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>GreenDrive</h1>
-      <Map />
     </div>
   );
 }
