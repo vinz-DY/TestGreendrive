e8d240e3a3cf0fd2568f496ff2e8b9858d68be03
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-12-21 14:58:19 +0100

    suppression de la div car inutile

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -8,33 +8,31 @@ import "./Map.css";
 function Map() {
   const terminalsData = useLoaderData();
   return (
-    <div>
-      <MapContainer center={[43.3, 5.4]} zoom={13}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <MarkerClusterGroup chunkedLoading>
-          {terminalsData.map((marker) => (
-            <Marker
-              position={[marker.ylatitude, marker.xlongitude]}
-              key={marker.id}
-            >
-              <Popup>
-                <h2>Informations sur la borne</h2>
-                <ul>
-                  <li>Adresse: {marker.city}</li>
-                  <li>Acessibilité: {marker.access}</li>
-                  <li>Type de prise: {marker.connectic_id}</li>
-                  <li>Puissance maximale: {marker.power}</li>
-                  <li>Accès à la borne: {marker.acces_recharge}</li>
-                </ul>
-              </Popup>
-            </Marker>
-          ))}
-        </MarkerClusterGroup>
-      </MapContainer>
-    </div>
+    <MapContainer center={[43.3, 5.4]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MarkerClusterGroup chunkedLoading>
+        {terminalsData.map((marker) => (
+          <Marker
+            position={[marker.ylatitude, marker.xlongitude]}
+            key={marker.id}
+          >
+            <Popup>
+              <h2>Informations sur la borne</h2>
+              <ul>
+                <li>Adresse: {marker.city}</li>
+                <li>Acessibilité: {marker.access}</li>
+                <li>Type de prise: {marker.connectic_id}</li>
+                <li>Puissance maximale: {marker.power}</li>
+                <li>Accès à la borne: {marker.acces_recharge}</li>
+              </ul>
+            </Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
+    </MapContainer>
   );
 }
 
