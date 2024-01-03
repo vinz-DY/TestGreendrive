ceddb7c3d0f94c0cc69215439d1d8daa335c8372
vinz-DY <vincentducamp259@hotmail.com>
2024-01-03 16:16:46 +0100

    Home header Css

---
+++ frontend/src/components/HeaderHome.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Car1 from "../assets/Car1.jpg";
+import "./headerHome.css";
+
+function HeaderHome() {
+  return (
+    <div>
+      <div className="headerContainer">
+        <div className="leftContainer">
+          <div className="SlogContainer">
+            <p>
+              Avec <span>GreenDrive</span>
+            </p>
+            <p className="Pindex">Partez en voyage avec votre véhicule</p>
+            <p>en toute serenité</p>
+          </div>
+          <div className="buttonsContainer">
+            <button type="button" className="button1">
+              S'inscrire
+            </button>
+            <div className="buttonsSignContainer">
+              <button type="button" className="buttonDeco">
+                deconnexion
+              </button>
+              <button type="button" className="buttonCo">
+                connexion
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <img className="headerCar" src={Car1} alt="header voiture" />
+      </div>
+    </div>
+  );
+}
+
+export default HeaderHome;
