3d59b17160230d2c02ecda3817179bff5cc58efe
sebastien b <bas.envoi2604@gmail.com>
2024-01-03 16:15:03 +0100

    modification base de donnee seed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,16 +6,16 @@ import App from "./App";
 import Map from "./components/Map/Map";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    loader: () => {
-      return connexion
-        .get(`${import.meta.env.VITE_BACKEND_URL}/api/terminals`)
-        .then((res) => res.data)
-        .catch((err) => console.error(err));
-    },
-  },
+  // {
+  //   path: "/",
+  //   element: <App />,
+  //   loader: () => {
+  //     return connexion
+  //       .get(`${import.meta.env.VITE_BACKEND_URL}/api/terminals`)
+  //       .then((res) => res.data)
+  //       .catch((err) => console.error(err));
+  //   },
+  // },
   {
     path: "/",
     element: <App />,
