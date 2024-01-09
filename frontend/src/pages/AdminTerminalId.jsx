663b5629751d12fc2621efebc745023e6c77f67e
sebastien b <bas.envoi2604@gmail.com>
2024-01-09 19:44:29 +0100

    button retour

--- frontend/src/pages/AdminTerminalId.jsx
+++ frontend/src/pages/AdminTerminalId.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import DisplayCards from "../components/DisplayCards";
 
 function AdminTerminalId() {
-  return <DisplayCards basePath="/terminals/:id" />;
+  return (
+    <div>
+      <DisplayCards basePath="/terminals/:id" />
+      <Link to="/admin/terminal">Retour</Link>
+    </div>
+  );
 }
 
 export default AdminTerminalId;
