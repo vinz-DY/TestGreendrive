f80eb090d1e555c2ddc57b2a508942bfccca93b4
sebastien b <bas.envoi2604@gmail.com>
2024-01-12 10:20:18 +0100

    page admin

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NavAdmin from "../components/NavAdmin";
+import Car1 from "../assets/Car1.jpg";
+import "./Admin.css";
 
 function Admin() {
   return (
-    <div>
-      <NavAdmin />
+    <div className="Admin_container">
+      <div>
+        <NavAdmin />
+      </div>
+      <div className="Admin_img">
+        <img src={Car1} alt="logo" />
+      </div>
     </div>
   );
 }
