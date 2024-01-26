33ad3847a57ba7803ce5a855b47cd86682b5c70c
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-01-26 10:12:47 +0100

    suppression des commentaires

--- frontend/src/components/Faq/Faq.jsx
+++ frontend/src/components/Faq/Faq.jsx
@@ -1,34 +1,3 @@
-// FEATURE FAQ
-// QUESTIONS
-//
-// 1- Par quel biais régler ma facture pour recharger mon véhicule ?
-// Le règlement de la facture peut se faire directement sur votre application mobile mais également sur la borne de recharge.
-// Attention cependant, le règlement ne peux être effectué que par carte bancaire.
-//
-// 2- Puis-je recharger mon véhicule même sous la pluie ou sous la neige ?
-// Pas d'inquiétude à ce sujet ! Les véhicules, câbles et bornes sont conçus pour supporter des conditions météo extrêmes.
-// Veillez cependant à retirer la neige autour des connecteurs avant de recharger votre véhicule.
-//
-// DURÉE CRÉNEAU RÉSERVATION
-// Quelle est la durée maximum de réservation d'une borne de recharge ?
-// Chaque réservation est prévue pour une durée de 30 minutes (durée nécessaire à la recharge de votre véhicule).
-//
-// CHARTE DE BONNE CONDUITE DE LA RECHARGE
-// Afin que tout le monde puisse profiter des bornes de recharge dans de bonnes conditions, veuillez suivre ces règles pleines de bon sens.
-// 1- Ne stationnez sur un emplacement de recharge que si vous rechargez. Personne n’aurait l’idée de stationner devant
-// une pompe à essence tout en faisant ses courses, il en est de même avec une borne de recharge.
-// 2. Quittez l’emplacement de recharge dès que votre véhicule est chargé. Laissez la place aux autres conducteurs pour que tout le monde puisse en proﬁter !
-// 3. Prenez soin des équipements. Ne quittez pas la borne sans avoir raccroché la prise de charge.
-// 4. Soyez courtois. Faites preuve de pédagogie face aux électromobilistes qui ne sont pas sensibilisés
-// à ces règles de bonne conduite mais également face aux usagers de véhicules thermiques qui ne respectent pas les places réservées aux véhicules électriques.
-//
-// MODIFICATION INFOS PROFIL
-// Si vous souhaitez modifier des informations sur votre profil, il faudra :
-// 1. Vous connecter sur votre compte
-// 2. Cliquer sur la carte "Profil"
-// 3. Sélectionnez la rubrique que vous souhaitez modifier
-// 4. Remplissez le formulaire avec les nouvelles informations
-
 import React from "react";
 import "./Faq.css";
 
