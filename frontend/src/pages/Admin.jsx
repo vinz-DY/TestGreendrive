4f95e7e1b4a1608d50cb83ddc4865d402352593e
sebastien b <bas.envoi2604@gmail.com>
2024-01-29 20:04:10 +0100

    redirection admin ok url

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 import { Outlet, Navigate } from "react-router-dom";
-import AuthProvider from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 import NavAdmin from "../components/NavAdmin";
 import "./Admin.css";
 
 function Admin() {
-  const { connected } = useContext(AuthProvider);
-  if (connected.role !== 1) {
-    return <Navigate to="/admin" replace />;
+  const { connected } = useContext(AuthContext);
+  if (connected.role === 1) {
+    return <Navigate to="/admin" />;
   }
   return (
     <div className="Admin_container">
