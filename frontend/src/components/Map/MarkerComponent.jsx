5fd5be326892c29611f853cac1b4f26c3d3b1510
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-02-05 11:31:27 +0100

    ajout du bouton réserver

--- frontend/src/components/Map/MarkerComponent.jsx
+++ frontend/src/components/Map/MarkerComponent.jsx
@@ -14,6 +14,9 @@ function MarkerComponent({ marker }) {
           <li>Puissance maximale: {marker.power}</li>
           <li>Accès à la borne: {marker.acces_recharge}</li>
         </ul>
+        <button type="button" className="search-button">
+          Réserver
+        </button>
       </Popup>
     </Marker>
   );
