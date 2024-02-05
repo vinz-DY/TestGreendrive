c7e92842781cd2db170fe9dab0c39ae2cd44fee8
vinz-DY <vincentducamp259@hotmail.com>
2024-02-05 15:13:05 +0100

    ajout des boutons modif et suppr avec css

--- frontend/src/pages/AdminTerminalId.jsx
+++ frontend/src/pages/AdminTerminalId.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import DisplayCardsId from "../components/DisplayCardsId";
+import "./AdminTerminalId.css";
 
 function AdminTerminalId() {
   return (
-    <div>
-      <DisplayCardsId basePath="/terminals/:id" />
+    <div className="adminTerminalBigCtn">
+      <div>
+        <DisplayCardsId basePath="/terminals/:id" />
+      </div>
+      <div className="adminTerminalBtnCtn">
+        <button className="adminTerminalBtn" type="button">
+          Modifier
+        </button>
+        <button className="adminTerminalBtn adminTerminalBtn2" type="button">
+          Supprimer
+        </button>
+      </div>
     </div>
   );
 }
