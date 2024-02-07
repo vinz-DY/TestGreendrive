f00f227dd218c1ff121e7e03ee0a07e1ad151f19
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-02-07 14:33:15 +0100

    modification du format d'affichage de la date et heure

--- frontend/src/components/Reservation/DisplayReserv.jsx
+++ frontend/src/components/Reservation/DisplayReserv.jsx
@@ -4,6 +4,53 @@ import "./DisplayReserv.css";
 
 function DisplayReserv() {
   const reserv = useLoaderData();
+
+  // Fonction de comparaison pour trier les réservations par date puis par heure
+  const compareReservations = (a, b) => {
+    // Convertir les dates en objets Date pour permettre la comparaison
+    const dateA = new Date(a.startTime);
+    const dateB = new Date(b.startTime);
+
+    // Comparaison des dates
+    if (dateA < dateB) return -1;
+    if (dateA > dateB) return 1;
+
+    // Si les dates sont égales, comparer les heures
+    const heureA = dateA.getHours();
+    const minuteA = dateA.getMinutes();
+    const heureB = dateB.getHours();
+    const minuteB = dateB.getMinutes();
+
+    // Comparaison des heures
+    if (heureA < heureB) return -1;
+    if (heureA > heureB) return 1;
+
+    // Si les heures sont égales, comparer les minutes
+    if (minuteA < minuteB) return -1;
+    if (minuteA > minuteB) return 1;
+
+    // Si les dates et heures sont identiques, pas besoin de changer l'ordre
+    return 0;
+  };
+
+  // Trier les réservations
+  reserv.sort(compareReservations);
+
+  const formatDate = (dateTimeString) => {
+    const dateTime = new Date(dateTimeString);
+    const day = dateTime.getDate();
+    const month = dateTime.getMonth() + 1;
+    const year = dateTime.getFullYear();
+    const hours = dateTime.getHours();
+    const minutes = dateTime.getMinutes();
+
+    // Ajout d'un 0 devant les minutes si elles sont inférieures à 10
+
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    return `${day}/${month}/${year} à ${hours}:${formattedMinutes}`;
+  };
+
   return (
     <div className="reserv-container">
       <h1 className="title-reserv">Historique des réservations</h1>
@@ -14,7 +61,8 @@ function DisplayReserv() {
               ID de la borne réservée: {reservation.terminal_id}
             </p>
             <p className="infos-reserv">
-              Date et heure de la réservation: {reservation.startTime}
+              Date et heure de la réservation: le
+              {formatDate(reservation.startTime)}
             </p>
           </div>
         ))}
